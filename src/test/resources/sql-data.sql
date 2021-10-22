3dafdf4632ac69aa23ed0fdd5c8a7c85baa17624
LukasR23 <Lukas@jazzcatz.com>
2021-10-22 16:49:33 +0100

    IMS-16 #comment quick last commit before I shut down for the night, I can't remember if the changes were put in properly.

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 set @person_id = LAST_INSERT_ID();
 INSERT INTO `ims`.`products` (`product_name`, `product_value`) VALUES ('Really big chair', '500.25');
-set @product_id = LAST_PRODUCT_ID();
+set @product_id = LAST_INSERT_ID();
 INSERT INTO `ims`.`orders` (`id`) SELECT `id` FROM `ims`.`customers` where `first_name` like 'jordan';
-set @order_id = LAST_ORDER_ID();
+set @order_id = LAST_INSERT_ID();
 INSERT INTO `ims`.`orders_items` (`order_id`, `product_id`) VALUES (@order_id, @product_id);
