6744d164545ecc3cbbc3d25f14a091952f90c82f
LukasR23 <Lukas@jazzcatz.com>
2021-10-25 17:13:15 +0100

    IMS-1 #comment customers works again. Moving on..

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,7 +1,9 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 set @person_id = LAST_INSERT_ID();
+INSERT INTO `customers` (`first_name`, `surname`) VALUES ('johnny', 'smithy');
 INSERT INTO `products` (`product_name`, `product_value`) VALUES ('Really big chair', '500.25');
 set @product_id = LAST_INSERT_ID();
+INSERT INTO `products` (`product_name`, `product_value`) VALUES ('Endless bag of wine', '1004.21');
 INSERT INTO `orders` (`id`, `delivery_reason`) VALUES (1, 'Art exhibit');
 set @order_id = LAST_INSERT_ID();
 INSERT INTO `orders_items` (`order_id`, `product_id`) VALUES (1, 1);
